e8129019c199de8d34c6b772ef438c2607e21104
James Circadian <jamescircadian@gmail.com>
2019-02-03 00:18:46 +0000

    Corrections to the sleep timing algorithm, only for speeding it up but inbiting slowing it down

--- pyremotenode/tasks/ts7400.py
+++ pyremotenode/tasks/ts7400.py
@@ -11,6 +11,8 @@ from pyremotenode.tasks import BaseTask
 class Sleep(BaseTask):
     def __init__(self, **kwargs):
         self._re_date = re.compile(r'^\d{8}$')
+        self._dt_format = "%d-%m-%Y"
+        self._tm_format = "%H:%M:%S"
         super(Sleep, self).__init__(**kwargs)
 
     def default_action(self,
@@ -39,80 +41,88 @@ class Sleep(BaseTask):
         # Evaluate minimum seconds, push to tomorrow if we've gone past the time today
         if seconds < 60:
             dt = dt + timedelta(days=1)
-            seconds = (datetime.combine(dt, tm) - datetime.utcnow()).total_seconds()
 
-        # Parse reboot time
+        # Parse reboot time and previously set sleep duration / time at which it was set
+        # TODO: The algorithm below is a bit shit, it would be better to incorporate the derived last_sleep_error
+        # and its sources into the calculation, but it's late and keeping it simple for the moment is best
+        # TODO: The reboot file needs to be POST-bootup time correction
         try:
             dt_reboot = self._get_reboot_time()
-            dt_reboot_set = self._get_reboot_set_time()
+            dt_expected = datetime.combine(dt_reboot.date(), tm)
+            dt_reboot_set, set_for = self._get_reboot_set_time()
+            last_sleep_error = int((dt_reboot - (dt_reboot_set + timedelta(seconds=set_for))).total_seconds())
         except Exception:
-            logging.warning("No satisfactory information to set adjustment offset")
+            logging.exception("No satisfactory information to set adjustment offset")
             dt_reboot = None
+            dt_expected = None
             dt_reboot_set = None
 
-        reboot_diff = 0
-        if dt_reboot and dt_reboot_set:
-            reboot_diff = int((dt_reboot_set - dt_reboot).total_seconds())
+        prev_error = 0
+        dt_to_wake = datetime.combine(dt, tm)
+        dt_now = datetime.utcnow()
 
-            logging.info("Difference between {} and {}: {} seconds".format(
-                dt_reboot.strftime("%H:%M:%S"),
-                dt_reboot_set.strftime("%H:%M:%S"),
-                reboot_diff))
+        # TODO: Because I don't trust this whole thing in relation to the RTC, this is a wee goody to retain the
+        # previous years ability to just wake up earlier and sit idly waiting for the day to start
+        if dt_reboot and dt_expected and dt_reboot_set and \
+           not os.path.exists(os.path.expandvars(os.path.join("$HOME", "NO_SLEEP_ADJUST"))):
+            prev_error = int((dt_reboot - dt_expected).total_seconds())
+            logging.info("Sleep error rate has been calculated at {} seconds".format(prev_error))
+            logging.info("The last sleep error was {}".format(last_sleep_error))
+
+            if prev_error < 0:
+                prev_error = 0
+                logging.info("We aren't going to slow down the sleep yet, setting offset to zero")
+
+        seconds = int((dt_to_wake - dt_now).total_seconds() - prev_error)
 
         TS7400Utils.rtc_clock()
         logging.info("Sleeping until {}, for {} seconds".format(
-            datetime.combine(dt, tm).strftime("%d/%m/%Y %H:%M:%S"), seconds))
-        iso_dt = datetime.utcnow()
-        iso_dt = iso_dt.replace(microsecond=0)
-        cmd = "goto_sleep {} {}".format(str(int(seconds + reboot_diff)), datetime.isoformat(iso_dt))
+            dt_to_wake.strftime("{} {}".format(self._dt_format, self._tm_format)), seconds))
+
+        cmd = "goto_sleep {} \"{}\"".format(
+            seconds, dt_now.strftime("{} {}".format(self._dt_format, self._tm_format)))
 
         logging.info("Running Sleep command: {}".format(cmd))
-        # TODO: We never see the last of the logs?
+        logging.shutdown()
+
         rc = sp.call(shlex.split(cmd))
 
-        if rc != 0:
-            # TODO: Handle this, should be extremely unlikely...
-            self.state = BaseTask.CRITICAL
-
-            logging.error("Did not manage to go to sleep, something's very wrong...")
-
-        self.state = BaseTask.OK
-        return self.state
+        raise RuntimeError("No way in hell we should be reaching this point, sleep rc: {}".format(rc))
 
     def _get_reboot_time(self):
         path = os.path.expandvars(os.path.join("$HOME", "reboot.txt"))
 
         if os.path.exists(path) and \
-                        (datetime.utcnow() - datetime.fromtimestamp(os.stat(path).st_mtime)).total_seconds() < 86400:
+                         (datetime.utcnow() - datetime.fromtimestamp(os.stat(path).st_mtime)).total_seconds() < 86400:
             with open(path, "r") as fh:
                 line = fh.readline().strip()
         else:
             return None
 
-        dt = self._parse_system_datetime(re.compile(r'^Rebooted at (.+)$'), line)
-        logging.debug("Unit was set to wake up at {}".format(datetime.isoformat(dt)))
+        dt = self._parse_system_datetime(re.compile(r'^(.+)$'), line)
+        logging.debug("Unit woke up at {}".format(
+            dt.strftime("{} {}".format(self._dt_format, self._tm_format))))
         return dt
 
     def _get_reboot_set_time(self):
         path = os.path.expandvars(os.path.join("$HOME", "sleepinfo.txt"))
 
-        # TODO: This only works with sleeping less than a day
+        # This only works with sleeping less than a day, which is what we want
         if os.path.exists(path) and \
-                        (datetime.utcnow() - datetime.fromtimestamp(os.stat(path).st_mtime)).total_seconds() < 2 * 86400:
+                         (datetime.utcnow() - datetime.fromtimestamp(os.stat(path).st_mtime)).total_seconds() < 2 * 86400:
             with open(path, "r") as fh:
                 line = fh.readline().strip()
-                (secs, dt) = line.split(",")
-            dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
-            dt = dt + timedelta(seconds=int(secs))
-            logging.debug("Unit was set to wake up at {}".format(datetime.isoformat(dt)))
-            return dt
+                (secs, dt_str) = line.split(",")
+            dt = datetime.strptime(dt_str, "{} {}".format(self._dt_format, self._tm_format))
+            logging.debug("Unit was set to sleep at {} for {} seconds".format(dt_str, secs))
+            return dt, int(secs)
         return None
 
     def _parse_system_datetime(self, regex, line):
         dt_match = regex.search(line)
 
         if dt_match:
-            return datetime.strptime(dt_match.group(1), "%a %b %d %H:%M:%S %Z %Y")
+            return datetime.strptime(dt_match.group(1), "{} {}".format(self._dt_format, self._tm_format))
         return None
 
 
@@ -126,13 +136,13 @@ class StatusUpdate(BaseTask):
 
 class TS7400Utils(object):
     @staticmethod
-    def rtc_clock(set = True):
-        type = "S" if set else "G"
-        logging.info("{}etting RTC from OS clock".format(type))
-        cmd = "tshwctl --{}etrtc".format(type.lower())
+    def rtc_clock(set=True):
+        action_type = "s" if set else "g"
+        logging.info("{}etting RTC from OS clock".format(action_type))
+        cmd = "tshwctl --{}etrtc".format(action_type)
 
         logging.debug("Running TS7400Utils command: {}".format(cmd))
         rc = sp.call(shlex.split(cmd))
 
         if rc != 0:
-            logging.warning("Did not manage to {}et RTC...".format(type.lower()))
+            logging.warning("Did not manage to {}et RTC...".format(action_type))
