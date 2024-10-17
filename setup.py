dc3209848253ca4fda48ed2c60c104c1346286f3
James Byrne <jambyr@bas.ac.uk>
2024-10-17 20:20:17 +0100

    Updating version to 0.6.0 line for certus

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "run_pyremotenode",
     ],
     include_package_data = True,
-    version = '0.5.0a2-lpm',
+    version = '0.6.0a3-lpm-certus',
     author = 'James Byrne',
     author_email = 'jambyr@bas.ac.uk',
     url = 'http://www.github.com/antarctica/pyremotenode',
