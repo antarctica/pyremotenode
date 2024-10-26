dc2b8189d61960abb007e858967d180146a5cd67
James Byrne <jambyr@bas.ac.uk>
2024-10-26 01:26:30 +0100

    Updating description for v0.6.0 bump

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as fh:
     reqs = fh.read().splitlines()
 
+with open("README.md") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyremotenode',
     packages=find_packages(),
@@ -16,6 +19,9 @@ setup(
     version="0.6.0",
     author="James Byrne",
     author_email="digitalinnovation@bas.ac.uk",
+    description="A service library for controlling low power devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://www.github.com/antarctica/pyremotenode',
     install_requires=reqs
 )
